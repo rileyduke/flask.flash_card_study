d20b029125c0fcaf7110de73dce7315994f45a84
Brendan Duke <brendanw.duke@gmail.com>
2020-02-15 07:20:32 -0500

    UI mockup start.

---
+++ frontend/ui-mockup/src/index.js
@@ -0,0 +1,20 @@
+import 'typeface-roboto';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
